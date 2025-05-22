a020be8cb4b8921b1f3db79d3ee147cc781626a1
Pervansh <big.bug.cha@gmail.com>
2025-05-22 21:54:00 +0300

    Interface update

--- Boltzmann_1D_Solver/solver/main.cpp
+++ Boltzmann_1D_Solver/solver/main.cpp
@@ -42,7 +42,13 @@ Bgk1dProblemData<T> temperatureRiemannProblem(T T_1, T T_2, int N_x, T x_max, in
 }
 
 template<std::floating_point T>
-Bgk1dProblemData<T> densityRiemannProblem(T Temp, T n_1, T n_2, int N_x, T x_max, int N_xi, T xi_max, T dt, T t_end, T delta) {
+Bgk1dProblemData<T> densityRiemannProblem(
+    T Temp, T n_1, T n_2, 
+    int N_x, T x_max, int N_xi, T xi_max, 
+    T dt, T t_end, 
+    T delta, 
+    T discont_point = T(0)
+) {
     assert(N_x % 2 == 0);
 
     Bgk1dProblemData<T> data;
@@ -56,6 +62,10 @@ Bgk1dProblemData<T> densityRiemannProblem(T Temp, T n_1, T n_2, int N_x, T x_max
     data.a = -x_max;
     data.b = x_max;
 
+    T dx = 2 * x_max / N_x;
+
+    int discont_x_ind = (int)std::round((discont_point - data.a) / dx);
+
     data.Ts = std::vector<T>(N_x);
 
     for (int x_i = 0; x_i < N_x; ++x_i) {
@@ -64,24 +74,54 @@ Bgk1dProblemData<T> densityRiemannProblem(T Temp, T n_1, T n_2, int N_x, T x_max
 
     data.f_1 = MaxwellDistribution1d<T>(data.Ts, data.a, data.b, N_xi, data.xi_max);
 
-    for (int x_i = 0; x_i < N_x / 2; ++x_i) {
+    for (int x_i = 0; x_i < discont_x_ind; ++x_i) {
         data.f_1[x_i] *= n_1;
     }
 
-    for (int x_i = N_x / 2; x_i < N_x; ++x_i) {
+    for (int x_i = discont_x_ind; x_i < N_x; ++x_i) {
         data.f_1[x_i] *= n_2;
     }
 
     return data;
 }
 
+void studentSpringDensityTest() {
+    double t_end = 5;
+    double dt = 0.001;
+    Bgk1dProblemData<double> data = densityRiemannProblem<double>(1., 2., 1., 150, 5., 45, 4.0, dt, t_end, 100.);
+
+    Full1dStateOutput output("output\\bgk1d\\testDensityMultiple");
+    const auto testRule = [dt](double t) { return (int)std::round(t / dt) % 100 == 0; };
+    bgk1dMethod<ExplicitEulerRK>(data, Cell1stOrderInt, output, testRule);
+}
+
+void freeMoleculeDensityTest() {
+    double t_end = 10;
+    double dt = 0.001;
+
+    const auto testRule = [dt](double t) { return (int)std::round(t / dt) % 100 == 0; };
+
+    /*
+    Bgk1dProblemData<double> data = densityRiemannProblem<double>(1., 2., 1., 150, 10., 46, 4.0, dt, t_end, 0.);
+    Full1dStateOutput output("output\\bgk1d\\testFreeMoleculeDensity");
+    bgk1dMethod<ExplicitEulerRK>(data, Cell1stOrderInt, output, testRule);
+    */
+
+    ///*
+    Bgk1dProblemData<double> shifted_data = densityRiemannProblem<double>(1., 2., 1., 150, 5., 224, 4.0, dt, t_end, 0., 1);
+    Full1dStateOutput shifted_output("output\\bgk1d\\testFreeMoleculeDensityShifted");
+    bgk1dMethod<ExplicitEulerRK>(shifted_data, Cell1stOrderInt, shifted_output, testRule);
+    //*/
+}
+
 int main() {
+    freeMoleculeDensityTest();
+
     /*
     Bgk1dProblemData<double> data = temperatureRiemannProblem<double>(1., 0.1, 6, 1., 5, 1.0, 0.01, 1., 1.);
 
     Macroparameters1dOutput output("output\\bgk1d\\testSmall");
     */
-
 
     //Bgk1dProblemData<double> data = temperatureRiemannProblem<double>(1., 0.1, 50, 1., 45, 4.0, 0.001, 0.1, 1.);
 
@@ -104,13 +144,6 @@ int main() {
     //Full1dStateOutput output("output\\bgk1d\\testDensity");
     //bgk1dMethod<ExplicitEulerRK>(data, Cell1stOrderInt, output);
 
-    double t_end = 5;
-    double dt = 0.001;
-    Bgk1dProblemData<double> data = densityRiemannProblem<double>(1., 2., 1., 150, 5., 45, 4.0, dt, t_end, 100.);
-
-    Full1dStateOutput output("output\\bgk1d\\testDensityMultiple");
-    const auto testRule = [dt](double t) { return (int)std::round(t / dt) % 100 == 0; };
-    bgk1dMethod<ExplicitEulerRK>(data, Cell1stOrderInt, output, testRule);
     /*
     std::cout << "Hello world!" << std::endl;
     
